50af1c9d6a2a55bc2bfca2007f5413ebb4652011
ThisIsLeia <96289708+ThisIsLeia@users.noreply.github.com>
2023-07-02 19:13:22 +0800

    新增上傳圖片功能

---
+++ future/detector/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from flask_wtf.form import FlaskForm
+from wtforms.fields.simple import SubmitField
+
+
+class UploadImageForm(FlaskForm):
+    # 在檔案欄位設定所需驗證
+    image = FileField(
+        validators=[
+            FileRequired('請指定圖片檔案'),
+            FileAllowed(['png', 'jpg', 'jpeg'], '不支援該圖片格式')
+        ]
+    )
+
+    submit = SubmitField('上傳')
